4695d4af96f799d02c80ec951d6c9ddf24f6d36f
fumiaki1103 <159741296+fumiaki1103@users.noreply.github.com>
2024-09-11 04:12:12 +0900

    初回コミット

--- html/index.php
+++ html/index.php
@@ -1,27 +1,29 @@
 <?php
-function max_number($n1, $n2) {
-    if ($n1 > $n2) {
-        return $n1;
-    } else {
-        return $n2;
+class Staff {
+    private $name;
+    private $age;
+    private $sex;
+    private $id;
+    private static $counter = 1;
+
+    public function __construct($name, $age, $sex) {
+        $this->name = $name;
+        $this->age = $age;
+        $this->sex = $sex;
+        $this->id = 'S' . str_pad(self::$counter, 4, '0', STR_PAD_LEFT);
+        self::$counter++;
+    }
+
+    public function show() {
+        printf("(%s)%s% d歳 %s<br>", $this->id, $this->name, $this->age, $this->sex);
     }
 }
 
-$a = 9;
-$b = 21;
+$staff1 = new Staff("佐藤 一郎", 31, "男性");
+$staff2 = new Staff("山田 花子", 25, "女性");
+$staff3 = new Staff("鈴木 次郎", 27, "男性");
 
-$max_value = max_number($a, $b);
+$staff1->show();
+$staff2->show();
+$staff3->show();
 ?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>最大値を求める</title>
-</head>
-<body>
-    <p>$a = <?php echo $a; ?></p>
-    <p> $b = <?php echo $b; ?></p>
-    <p>$aと$bのうち最大値は<?php echo $max_value; ?>です。</p>
-</body>
-</html>
